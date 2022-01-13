ca417f4944263269e561a410f418937b26fde6c2
raynavarma <97682448+raynavarma@users.noreply.github.com>
2022-01-13 09:57:41 -0500

    Update Arena.java

--- Arena.java
+++ Arena.java
@@ -1,1 +1,17 @@
+public class Arena
+{
+    private int temp;
+    private String type;
+    private boolean hasgravity;
+    private boolean hasTrees;
+    private boolean hasCars;
 
+    
+    
+    public void Arena(int temp, String type, boolean hasGravity, boolean hasTrees, boolean hasCars)
+    {
+        
+    }
+
+
+}
