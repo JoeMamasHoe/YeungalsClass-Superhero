f9cc88e861fc69cb993fd40a3d36a265ec4f1901
murc0 <92402819+murc0@users.noreply.github.com>
2022-01-14 10:08:47 -0500

    -added main

--- battlePass.java
+++ battlePass.java
@@ -1,5 +1,47 @@
 public class battlePass
 {
+  public void main(String args[])
+  {
+      p("You enter the merchant's shop.");
+      sleep(1000);
+      p("The walls are lined with foreign items you do not recognize...");
+      sleep(1000);
+      p("The merchant greets you");
+      sleep(2000);
+      enterSound();
+      items();
+      
+  }
+  public void sleep(int ms)
+  {
+    System.out.println("First print.");
+    try 
+    {
+        Thread.sleep(ms); //2 secs
+    }
+    catch (InterruptedException e) 
+    {
+        e.printStackTrace();
+    }
+    System.out.println("This one comes after 2 seconds from the println.");
+  }
+  public void items()
+  {
+      ArrayList <String> itemList = new ArrayList();
+      //itemList.add
+      //itemList.add       add items in battle pass here
+      //itemList.add
+      
+      //checks players highest lvl in tower
+      
+      //adds abilities to Fighter ArrayList based on lvl
+      
+      leaveSound();
+  }
+  public void p(String x)
+  {
+      System.out.println(x);
+  }
   public void enterSound
   {
      //ryan welcomes you to the battle pass
