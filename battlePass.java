2432b2bb9b4b58694377246ceeb06cc24490f251
murc0 <92402819+murc0@users.noreply.github.com>
2022-01-14 12:56:37 -0500

    updated main method and added working placeholder sounds

--- battlePass.java
+++ battlePass.java
@@ -1,30 +1,54 @@
- public class battlePass
+import java.util.*;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+public class battlePass
 {
-  public void main(String args[])
+  public static void main(String args[])throws UnsupportedAudioFileException, LineUnavailableException, IOException
   {
+      Scanner in = new Scanner (System.in);
       p("You enter the merchant's shop.");
-      sleep(1000);
-      p("The walls are lined with foreign items you do not recognize...");
-      sleep(1000);
-      p("The merchant greets you");
       sleep(2000);
+      p("The walls are lined with foreign items you do not recognize...");
+      sleep(2000);
+      p("The merchant greets you");
       enterSound();
-      items();
-      
+      sleep(2500);
+      p("Press C to continue");
+      String key3 = in.next();
+      if (key3.equalsIgnoreCase("C"))
+      {
+        items();
+      }
   }
-  public void sleep(int ms)
+  public static void enterSound() throws UnsupportedAudioFileException, LineUnavailableException, IOException
   {
-    try 
-    {
-        Thread.sleep(ms); //2 secs
-    }
-    catch (InterruptedException e) 
-    {
-        e.printStackTrace();
-    }
+     //ryan says hello
+     //Scanner in = new Scanner (System.in);
+     File file = new File("vineboom.wav"); //temporary sound
+     AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+     Clip clip = AudioSystem.getClip();
+     clip.open(audioStream);
+     
+     clip.start();
+     
+     //String response = in.next();
   }
-  public void items()
+  public static void sleep(int ms)
   {
+     try 
+     {
+         Thread.sleep(ms);
+     }
+         catch (InterruptedException e) 
+     {
+         e.printStackTrace();
+     }
+  }
+  public static void items() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+  {
+      Scanner in = new Scanner (System.in);
+      p("items");
       ArrayList <String> itemList = new ArrayList();
       //itemList.add
       //itemList.add       add items in battle pass here
@@ -33,19 +57,27 @@
       //checks players highest lvl in tower
       
       //adds abilities to Fighter ArrayList based on lvl
-      
+      sleep(1000);
+      p("The merchant wishes you well...");
+      p("Press any key to continue");
       leaveSound();
+      String key4 = in.next();
   }
-  public void p(String x)
+  public static void p(String x)
   {
       System.out.println(x);
   }
-  public void enterSound
+  public static void leaveSound() throws UnsupportedAudioFileException, LineUnavailableException, IOException
   {
-     //ryan welcomes you to the battle pass
-  }
-  public void leaveSound
-  {
-    //ryan says goodbye 
+     //ryan says goodbye 
+     //Scanner in = new Scanner (System.in);
+     File file = new File("vineboom.wav"); //temporary sound
+     AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+     Clip clip = AudioSystem.getClip();
+     clip.open(audioStream);
+     
+     clip.start();
+     
+     //String response = in.next();
   }
 }
