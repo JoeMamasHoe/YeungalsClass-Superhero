0ee7bc5ea142c9b826a99cc45b5f2f190e41bae0
raynavarma <97682448+raynavarma@users.noreply.github.com>
2022-01-14 10:08:05 -0500

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,3 +1,24 @@
 public class Hero extends Fighters{
+
+    private double health;
+    private int damage;
+    private double arenaDamageMultiplier;
+    private String name;
+
+    public Hero(String name, double arenaDamageMultiplier){
+        super(true,false);
+    }
+
+    public void punch(){
+        System.out.println("The hero throws a fierce punch!");
+    }
+
+    public void kick(){
+        System.out.println("The hero kicks with a mighty force!");
+    }
+
+    public void special(){
+        System.out.println("The hero uses their special move!");
+    }
   
 }
