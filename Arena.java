b2ec34b1c76dc8e65f28796220f736c0b0a00eca
juliuscaesar-12 <73482125+juliuscaesar-12@users.noreply.github.com>
2022-01-14 09:38:57 -0500

    Encapsulation in Arena.java
    lines 23-70 getters and setters

--- Arena.java
+++ Arena.java
@@ -2,20 +2,72 @@ public class Arena
 {
     private int temp;
     private String type;
-    private boolean hasgravity;
+    private boolean hasGravity;
     private boolean hasTrees;
     private boolean hasCars;
 
+    public Arena()
+    {
+    }
     
-    
-    public void Arena(int temp, String type, boolean hasGravity, boolean hasTrees, boolean hasCars)
+    public Arena(int temp, String type, boolean hasGravity, boolean hasTrees, boolean hasCars)
     {
         
     }
     
-    public void distraction(){
+    public void distraction()
+    {
         System.out.println("Fighter gets hit");
     }
-
+    
+    public int getTemp()
+    {
+        return temp;
+    }
+    
+    public void setTemp(int x)
+    {
+        temp = x;
+    }
+    
+    public String getType()
+    {
+        return type;
+    }
+    
+    public void setType(String y)
+    {
+        type = y;
+    }
+    public String getGravity()
+    {
+        return hasGravity;
+    }
+    
+    public void setGravity(boolean y)
+    {
+        hasGravity = y;
+    }
+    
+    public String getTrees()
+    {
+        return hasTrees;
+    }
+    
+    public void setTrees(boolean y)
+    {
+        hasTrees = y;
+    }
+    
+    public String getCars()
+    {
+        return hasCars;
+    }
+    
+    public void setCars(boolean y)
+    {
+        hasCars = y;
+    }
+    
 
 }
