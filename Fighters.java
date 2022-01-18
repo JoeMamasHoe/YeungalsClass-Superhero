97f7d7ab58a22e77fa69c67989556ce0a63c08e1
adamgeo1 <69363323+adamgeo1@users.noreply.github.com>
2022-01-18 07:51:44 -0500

    update fighters with constructor and some moves

--- Fighters.java
+++ Fighters.java
@@ -1,7 +1,34 @@
 public class Fighters
 {
-  private int health;
+  private double health;
+  private int damage;
+  private double arenaDamageMultiplier;
   private String name;
   private boolean isHero;
   private boolean isVillain;
+  private String preferredMap;
+  private String type;
+
+  public Fighters(boolean isHero, boolean isVillain){
+    health = 100.0;
+    name = "Default";
+    damage = 10;
+    arenaDamageMultiplier = 1.0;
+    preferredMap = "City";
+    type = "Human";
+    this.isHero = isHero;
+    this.isVillain = isVillain;
+  }
+
+  public void punch(){
+    System.out.println("The fighter throws a fierce punch!");
+  }
+
+  public void kick(){
+    System.out.println("The fighter kicks with a mighty force!");
+  }
+
+  public void special(){
+    System.out.println("The fighter uses their special move!");
+  }
 }
