1681a13c49f988431860d6b33d97374fc4efaf2d
raynavarma <97682448+raynavarma@users.noreply.github.com>
2022-01-18 10:07:57 -0500

    Update DetectiveMatt.java

--- DetectiveMatt.java
+++ DetectiveMatt.java
@@ -1,4 +1,17 @@
 public class DetectiveMatt extends Hero{
+    
+    public DetectiveMatt(){
+        super("DetectiveMatt", 1.0);
+        
+    }
+    public void punch(){
+        System.out.println("Detective Matt throws a weak punch!");
+    }
+
+    public void kick(){
+        System.out.println("Detective Matt kicks with an insane force!");
+    }
+    
     public void special(){
         System.out.println("Detective Matt takes out a gun");
     }
