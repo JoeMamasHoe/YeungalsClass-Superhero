ed9b73c6b7700e4a1537b38346acf6f004951029
raynavarma <97682448+raynavarma@users.noreply.github.com>
2022-01-18 10:10:03 -0500

    Update MrRobot.java

--- MrRobot.java
+++ MrRobot.java
@@ -1,5 +1,17 @@
 public class MrRobot extends Hero{
     
+    public MrRobot(){
+        super("MrRobot", 1.0);
+        
+    }
+    public void punch(){
+        System.out.println("MrRobot throws a little punch!");
+    }
+
+    public void kick(){
+        System.out.println("MrRobot kicks with a fierce force!");
+    }
+    
     public void special(){
         System.out.println("Mr. Robot transforms into a tank");
     }
