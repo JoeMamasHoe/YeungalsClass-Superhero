68f36eeb54ff9231a916ffdd692f90c31fa36de1
raynavarma <97682448+raynavarma@users.noreply.github.com>
2022-01-18 09:45:10 -0500

    Update GustavoFabiano.java

--- GustavoFabiano.java
+++ GustavoFabiano.java
@@ -3,5 +3,9 @@ public class GustavoFabiano extends Villain{
     public GustavoFabiano(){
         super("Gustavo Fabiano", 1.0);
         
+        public void special(){
+        System.out.println("Pulls out tommy gun");
+    }
+        
     }
 }
