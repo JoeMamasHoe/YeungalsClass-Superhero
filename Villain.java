a1b1bd4ee49324c152f34660a7b0881e90f0ec83
adamgeo1 <69363323+adamgeo1@users.noreply.github.com>
2022-01-18 07:55:20 -0500

    update villain
    added instance variables, constructor, and override move methods.

--- Villain.java
+++ Villain.java
@@ -1,3 +1,22 @@
-public class Vilain extends Fighters{
+public class Villain extends Fighters{
+    private double health;
+    private int damage;
+    private double arenaDamageMultiplier;
+    private String name;
 
+    public Villain(String name, double arenaDamageMultiplier){
+        super(false,true);
+    }
+
+    public void punch(){
+        System.out.println("The villain throws a fierce punch!");
+    }
+
+    public void kick(){
+        System.out.println("The villain kicks with a mighty force!");
+    }
+
+    public void special(){
+        System.out.println("The villain uses their special move!");
+    }
 }
