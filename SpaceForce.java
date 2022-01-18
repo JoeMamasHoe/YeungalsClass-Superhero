08b13b3bc27761986286083630496c4871d124a0
raynavarma <97682448+raynavarma@users.noreply.github.com>
2022-01-18 10:09:03 -0500

    Update SpaceForce.java

--- SpaceForce.java
+++ SpaceForce.java
@@ -1,5 +1,17 @@
 public class SpaceForce extends Hero{
     
+    public SpaceForce(){
+        super("SpaceForce", 1.0);
+        
+    }
+    public void punch(){
+        System.out.println("SpaceForce throws a crazy punch!");
+    }
+
+    public void kick(){
+        System.out.println("SpaceForce kicks with a slow force!");
+    }
+    
     public void special(){
         System.out.println("SpaceForce throws Rocket Barrage");
     }
