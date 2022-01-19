1dc4b8be9a9bad2ba350005995bb61dbf6c572e4
murc0 <92402819+murc0@users.noreply.github.com>
2022-01-18 20:01:25 -0500

    Update BattlePass.java

--- BattlePass.java
+++ BattlePass.java
@@ -1,11 +1,14 @@
+
 import java.util.*;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
-public class BattlePass
+public class battlePass
 {
   public static void main(String args[])throws UnsupportedAudioFileException, LineUnavailableException, IOException
   {
+      int towerlvl = 0;
+      boolean mulroy = false;
       Scanner in = new Scanner (System.in);
       p("You enter the merchant's shop.");
       sleep(1000);
@@ -16,25 +19,112 @@ public class BattlePass
       p("The merchant greets you");
       enterSound();
       sleep(2500);
+      if (towerlvl >= 3)
+      {
+          p("You beat tower level 3!");
+          sleep(1500);
+          p("You unlocked Yuengel's 2nd monitor!");
+          ryLore1(); //ry talks about the monitor
+          sleep(10000);
+      }
+      else
+      {
+          p("Level 3 Reward: Locked");
+      }
+      if (towerlvl >= 5)
+      {
+          p("You beat tower level 5!");
+          sleep(1500);
+          p("You unlocked a free level!");
+          towerlvl += 1;
+          p("You are now level 6!");
+          ryLore2();
+          sleep(10000);
+      }
+      else
+      {
+          p("Level 5 Reward: Locked");
+      }
+      if (towerlvl >= 8)
+      {
+          p("You beat tower level 8!");
+          sleep(1500);
+          p("You unlocked ");
+          ryLore3();
+          sleep(10000);
+      }
+      else
+      {
+          p("Level 8 Reward: Locked");
+      }
+      if (towerlvl >= 10)
+      {
+          p("You beat tower level 10!");
+          sleep(1500);
+          p("You unlocked a Secret  Fighter!");
+          p("Mr. Mulroy is available in the character select screen!");
+          mulroy = true;
+          ryLore4();
+          sleep(10000);
+      }
+      else
+      {
+          p("Level 10 Reward: Locked");
+      }
       p("Press C to continue");
       String key3 = in.next();
       if (key3.equalsIgnoreCase("C"))
       {
-        items();
+        sleep(1000);
+        p("The merchant wishes you well...");
+        p("Press any key to continue");
+        leaveSound();
       }
   }
-  public static void shopTheme() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+  public static void ryLore1() throws UnsupportedAudioFileException, LineUnavailableException, IOException
   {
-     //ryan says hello
-     //Scanner in = new Scanner (System.in);
-     File file = new File("kelpgtrim.wav"); //temporary sound
+     File file = new File("vineboom.wav"); //temporary sound
      AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
      Clip clip = AudioSystem.getClip();
      clip.open(audioStream);
      
      clip.start();
+  }
+  public static void ryLore2() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+  {
+     File file = new File("vineboom.wav"); //temporary sound
+     AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+     Clip clip = AudioSystem.getClip();
+     clip.open(audioStream);
      
-     //String response = in.next();
+     clip.start();
+  }
+  public static void ryLore3() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+  {
+     File file = new File("vineboom.wav"); //temporary sound
+     AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+     Clip clip = AudioSystem.getClip();
+     clip.open(audioStream);
+     
+     clip.start();
+  }
+  public static void ryLore4() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+  {
+     File file = new File("vineboom.wav"); //temporary sound
+     AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+     Clip clip = AudioSystem.getClip();
+     clip.open(audioStream);
+     
+     clip.start();
+  }
+  public static void shopTheme() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+  {
+     File file = new File("kelpgtrim.wav");
+     AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+     Clip clip = AudioSystem.getClip();
+     clip.open(audioStream);
+     
+     clip.start();
   }
   public static void enterSound() throws UnsupportedAudioFileException, LineUnavailableException, IOException
   {
@@ -63,7 +153,7 @@ public class BattlePass
   public static void items() throws UnsupportedAudioFileException, LineUnavailableException, IOException
   {
       Scanner in = new Scanner (System.in);
-      p("items");
+      
       ArrayList <String> itemList = new ArrayList();
       //itemList.add
       //itemList.add       add items in battle pass here
@@ -84,15 +174,12 @@ public class BattlePass
   }
   public static void leaveSound() throws UnsupportedAudioFileException, LineUnavailableException, IOException
   {
-     //ryan says goodbye 
-     //Scanner in = new Scanner (System.in);
+     //ryan says goodbye
      File file = new File("vineboom.wav"); //temporary sound
      AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
      Clip clip = AudioSystem.getClip();
      clip.open(audioStream);
      
      clip.start();
-     
-     //String response = in.next();
   }
 }
