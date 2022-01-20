3c3687708324ec56ed6d76a2d2dd37d65695bf16
adamgeo1 <69363323+adamgeo1@users.noreply.github.com>
2022-01-20 09:19:55 -0500

    Update BlueJay.java

--- BlueJay.java
+++ BlueJay.java
@@ -1,4 +1,14 @@
-public class BlueJay {
+public class BlueJay extends Villain {
+    private double punchDamage;
+    private double kickDamage;
+    private double specialDamage;
+
+    public BlueJay(){
+        super("BlueJay");
+        punchDamage = 20.0;
+        specialDamage = 40.0;
+        kickDamage = 15.0;
+    }
   
   public void special(){
         System.out.println("Blue Jay screams");
