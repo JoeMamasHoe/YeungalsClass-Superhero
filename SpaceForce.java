87e206a9a0fd7c649fd5b329a8d9f940b50a19bd
adamgeo1 <69363323+adamgeo1@users.noreply.github.com>
2022-01-20 09:23:03 -0500

    Update SpaceForce.java

--- SpaceForce.java
+++ SpaceForce.java
@@ -1,9 +1,17 @@
 public class SpaceForce extends Hero{
-    
+
+    private double punchDamage;
+    private double kickDamage;
+    private double specialDamage;
+
     public SpaceForce(){
-        super("SpaceForce", 1.0);
-        
+        super("SpaceForce");
+        punchDamage = 15.0;
+        specialDamage = 40.0;
+        kickDamage = 20.0;
     }
+
+
     public void punch(){
         System.out.println("SpaceForce throws a crazy punch!");
     }
