6475fe43367c98856bf699bb24ab109f1654ef52
adamgeo1 <69363323+adamgeo1@users.noreply.github.com>
2022-01-20 09:21:54 -0500

    Update MidKnight.java

--- MidKnight.java
+++ MidKnight.java
@@ -1,7 +1,19 @@
 public class MidKnight extends Hero{
-    
-    public void special(){
+
+    private double punchDamage;
+    private double kickDamage;
+    private double specialDamage;
+
+    public MidKnight(){
+        super("MidKnight");
+        punchDamage = 10.0;
+        specialDamage = 40.0;
+        kickDamage = 20.0;
+    }
+
+
+    public void special() {
         System.out.println("Midknight does big sword slash");
     }
-    
+
 }
