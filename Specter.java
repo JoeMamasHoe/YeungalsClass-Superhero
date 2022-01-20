07aff31b471ba18f719ed18cc86c7d91e717ddcd
adamgeo1 <69363323+adamgeo1@users.noreply.github.com>
2022-01-20 09:23:34 -0500

    Update Specter.java

--- Specter.java
+++ Specter.java
@@ -1,4 +1,15 @@
-public class Specter {
+public class Specter extends Villain{
+
+    private double punchDamage;
+    private double kickDamage;
+    private double specialDamage;
+
+    public Specter(){
+        super("Specter");
+        punchDamage = 20.0;
+        specialDamage = 40.0;
+        kickDamage = 15.0;
+    }
   
   public void special(){
         System.out.println("Specter makes a Shuriken");
