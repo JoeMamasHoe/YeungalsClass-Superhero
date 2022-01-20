83607ab0225b1f69d1c8793c12eff5e214ff0a78
adamgeo1 <69363323+adamgeo1@users.noreply.github.com>
2022-01-20 09:21:02 -0500

    Update GustavoFabiano.java

--- GustavoFabiano.java
+++ GustavoFabiano.java
@@ -1,8 +1,14 @@
 public class GustavoFabiano extends Villain{
-    
+
+    private double punchDamage;
+    private double kickDamage;
+    private double specialDamage;
+
     public GustavoFabiano(){
-        super("Gustavo Fabiano", 1.0);
-        
+        super("GustavoFabiano");
+        punchDamage = 15.0;
+        specialDamage = 40.0;
+        kickDamage = 20.0;
     }
     
     public void special(){
