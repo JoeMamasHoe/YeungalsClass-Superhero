3cb67cd7b299091dd25303f8f4a9fe07be919c36
murc0 <92402819+murc0@users.noreply.github.com>
2022-01-21 11:40:09 -0500

    added baseHit sound to punch and kick

--- Hero.java
+++ Hero.java
@@ -1,22 +1,38 @@
-public class Hero extends Fighters{
-
-    public Hero(String name){
-
+import java.util.*;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+public class Hero extends Fighters
+{
+    public Hero(String name)
+    {
         super(true,false, name);
     }
 
-
-    public void punch(){
-        System.out.println("The hero throws a fierce punch!");
+    public void baseHit() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       File file = new File("baseHit.wav");
+       AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+       Clip clip = AudioSystem.getClip();
+       clip.open(audioStream);
+       
+       clip.start();
+    }
+    
+    public void punch() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       System.out.println("The hero throws a fierce punch!");
+       baseHit();
     }
 
-    public void kick(){
+    public void kick() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
         System.out.println("The hero kicks with a mighty force!");
+        baseHit();
     }
 
-    public void special(){
+    public void special() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
         System.out.println("The hero uses their special move!");
     }
-  
 }
-, name
