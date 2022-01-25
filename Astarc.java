a6d869164978df3b48ec729d0bd95969dfd13e48
MattBartie <97681033+MattBartie@users.noreply.github.com>
2022-01-24 19:56:36 -0500

    Update Astarc.java

--- Astarc.java
+++ Astarc.java
@@ -1,16 +1,7 @@
 public class Astarc extends Villain{
-
-    private double punchDamage;
-    private double kickDamage;
-    private double specialDamage;
-
-    public Astarc(){
-        super("Astarc");
-        punchDamage = 15.0;
-        specialDamage = 40.0;
-        kickDamage = 20.0;
+public Astarc(){
+        super("Astarc", 20, 15, 40);
     }
-    
     public void special(){
         System.out.println("Acid spit");
     }
