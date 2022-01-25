c74693230c167810c1b775b63ef983f9d7226b3b
murc0 <92402819+murc0@users.noreply.github.com>
2022-01-25 09:13:43 -0500

    added gusSound

--- GustavoFabiano.java
+++ GustavoFabiano.java
@@ -1,9 +1,31 @@
+import java.util.*;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 public class GustavoFabiano extends Villain{
     public GustavoFabiano(){
         super("GustavoFabiano", 20, 15, 40);
     }
     public void special(){
         System.out.println("Pulls out tommy gun");
+        gusSound();
     }
-
+    public void baseHit() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       File file = new File("baseHit.wav");
+       AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+       Clip clip = AudioSystem.getClip();
+       clip.open(audioStream);
+       
+       clip.start();
+    }
+    public void gusSound() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       File file = new File("gusSpecial.wav");
+       AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+       Clip clip = AudioSystem.getClip();
+       clip.open(audioStream);
+       
+       clip.start();
+    }
 }
