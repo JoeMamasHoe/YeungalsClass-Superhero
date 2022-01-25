aee3d5ce83511ea1bd189f49853b864cb1eef24b
MattBartie <97681033+MattBartie@users.noreply.github.com>
2022-01-24 20:00:25 -0500

    Update GustavoFabiano.java

--- GustavoFabiano.java
+++ GustavoFabiano.java
@@ -1,15 +1,7 @@
 public class GustavoFabiano extends Villain{
 
-    private double punchDamage;
-    private double kickDamage;
-    private double specialDamage;
-
     public GustavoFabiano(){
-        super("GustavoFabiano");
-        punchDamage = 15.0;
-        specialDamage = 40.0;
-        kickDamage = 20.0;
-    }
+        super("GustavoFabiano", 15, 20, 40);
     
     public void special(){
         System.out.println("Pulls out tommy gun");
