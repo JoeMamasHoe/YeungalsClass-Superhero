88b5d4f8a80d171b4acb0f33fae5fecdf0756a9e
murc0 <92402819+murc0@users.noreply.github.com>
2022-01-25 09:10:51 -0500

    added astarcSound

--- Astarc.java
+++ Astarc.java
@@ -1,8 +1,32 @@
+import java.util.*;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 public class Astarc extends Villain{
-public Astarc(){
+    public Astarc(){
         super("Astarc", 20, 15, 40);
     }
-    public void special(){
+    public void special() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
         System.out.println("Acid spit");
+        astarcSound();
+    }
+    public void baseHit() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       File file = new File("baseHit.wav");
+       AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+       Clip clip = AudioSystem.getClip();
+       clip.open(audioStream);
+       
+       clip.start();
+    }
+    public void astarcSound() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       File file = new File("astarc.wav");
+       AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+       Clip clip = AudioSystem.getClip();
+       clip.open(audioStream);
+       
+       clip.start();
     }
 }
