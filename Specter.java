cf93f788be194510db7595467bf670ad39bb0210
MattBartie <97681033+MattBartie@users.noreply.github.com>
2022-01-24 20:04:59 -0500

    Update Specter.java

--- Specter.java
+++ Specter.java
@@ -1,14 +1,7 @@
 public class Specter extends Villain{
 
-    private double punchDamage;
-    private double kickDamage;
-    private double specialDamage;
-
     public Specter(){
-        super("Specter");
-        punchDamage = 20.0;
-        specialDamage = 40.0;
-        kickDamage = 15.0;
+        super("Specter", 20, 15, 40);
     }
   
   public void special(){
