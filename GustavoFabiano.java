6d0d553afda23ddc8af29e8e102e14222cd6d3de
MattBartie <97681033+MattBartie@users.noreply.github.com>
2022-01-24 20:36:06 -0500

    Update GustavoFabiano.java

--- GustavoFabiano.java
+++ GustavoFabiano.java
@@ -1,10 +1,9 @@
 public class GustavoFabiano extends Villain{
-
     public GustavoFabiano(){
-        super("GustavoFabiano", 15, 20, 40);
-    
+        super("GustavoFabiano", 20, 15, 40);
+    }
     public void special(){
         System.out.println("Pulls out tommy gun");
     }
-    
+
 }
