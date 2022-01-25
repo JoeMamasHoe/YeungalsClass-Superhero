10c81d64defc36f39fde5061a81ab8f5534d05bc
murc0 <92402819+murc0@users.noreply.github.com>
2022-01-25 09:22:55 -0500

    added midSound

--- MidKnight.java
+++ MidKnight.java
@@ -1,11 +1,31 @@
+import java.util.*;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 public class MidKnight extends Hero{
-
     public MidKnight(){
         super("MidKnight", 10, 20, 40);
     }
-
-    public void special() {
+    public void special() throws UnsupportedAudioFileException, LineUnavailableException, IOException{
         System.out.println("Midknight does big sword slash");
+        midSound();
     }
-
+    public void baseHit() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       File file = new File("baseHit.wav");
+       AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+       Clip clip = AudioSystem.getClip();
+       clip.open(audioStream);
+       
+       clip.start();
+    }
+    public void midSound() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       File file = new File("gusSpecial.wav");
+       AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+       Clip clip = AudioSystem.getClip();
+       clip.open(audioStream);
+       
+       clip.start();
+    }
 }
