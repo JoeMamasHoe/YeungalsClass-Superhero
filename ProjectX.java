448340790f698eb94d98e182387f24ba4d469cd7
murc0 <92402819+murc0@users.noreply.github.com>
2022-01-25 09:27:33 -0500

    added projSound

--- ProjectX.java
+++ ProjectX.java
@@ -1,9 +1,32 @@
+import java.util.*;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 public class ProjectX extends Villain{
-    public ProjectX(){
+    public ProjectX()
+    {
         super("ProjectX", 20, 15, 40);
     }
-    
-    public void special(){
+    public void baseHit() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       File file = new File("baseHit.wav");
+       AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+       Clip clip = AudioSystem.getClip();
+       clip.open(audioStream);
+       
+       clip.start();
+    }
+    public void projSound() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       File file = new File("ProjectX.wav");
+       AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+       Clip clip = AudioSystem.getClip();
+       clip.open(audioStream);
+       
+       clip.start();
+    }
+    public void special() throws UnsupportedAudioFileException, LineUnavailableException, IOException{
         System.out.println("Laser beam");
+        projSound();
     }
 }
