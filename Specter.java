a8d214bba46781cb47e2c567929803461e405b0b
murc0 <92402819+murc0@users.noreply.github.com>
2022-01-25 09:35:23 -0500

    added specSound

--- Specter.java
+++ Specter.java
@@ -1,10 +1,24 @@
-public class Specter extends Villain{
-
-    public Specter(){
-        super("Specter", 20, 15, 40);
+import java.util.*;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+public class Specter extends Villain
+{
+  public Specter(){
+      super("Specter", 20, 15, 40);
+  }
+  public void specSound() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+    {
+       File file = new File("specter.wav");
+       AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+       Clip clip = AudioSystem.getClip();
+       clip.open(audioStream);
+       
+       clip.start();
     }
-  
-  public void special(){
+  public void special() throws UnsupportedAudioFileException, LineUnavailableException, IOException
+  {
         System.out.println("Specter makes a Shuriken");
+        specSound();
     }
 }
